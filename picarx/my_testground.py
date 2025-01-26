8de2758d13a88e3b97ab0762a93f680e2dde4430
Roger Wienaah <rogerwienaah@gmail.com>
2025-01-25 17:17:26 -0800

    updated testground

--- picarx/my_testground.py
+++ picarx/my_testground.py
@@ -7,16 +7,20 @@ px = Picarx()
 def basic_maneuvering():
     px.set_dir_servo_angle(0)
     px.forward(50)
-    sleep(0.5)
+    sleep(1)
 
 
     px.set_dir_servo_angle(45)
     px.forward(50)
-    sleep(0.5)
+    sleep(2)
 
     px.set_dir_servo_angle(-45)
     px.forward(50)
-    sleep(0.5)
+    sleep(2)
+
+    px.set_dir_servo_angle(0)
+    px.forward(50)
+    sleep(1)
 
     px.stop()
 
