7961cc604f5438ac7054fa7773a8e55beabab4fb
Roger Wienaah <rogerwienaah@gmail.com>
2025-02-02 15:23:00 -0800

    line follower update

--- picarx/line_with_camera.py
+++ picarx/line_with_camera.py
@@ -1,15 +1,24 @@
 import numpy as np
 import cv2
 from picarx_improved import Picarx
-
-
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 import time
+
+
+
 picam2 = Picamera2()
+
+
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
-picam2.start_preview(Preview.DRM)
 picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+
+
+while True:
+    img = picam2.capture_array()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("PiCam", img)
+    cv2.waitKey(1)
+
 
