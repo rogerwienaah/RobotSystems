fd14dc18d72b3991467ddc4477f4ca2cd9db462f
Roger Wienaah <rogerwienaah@gmail.com>
2025-02-02 15:39:49 -0800

    line follower update

--- picarx/line_with_camera.py
+++ picarx/line_with_camera.py
@@ -9,7 +9,7 @@ import time
 picam2 = Picamera2()
 
 
-camera_config = picam2.create_preview_configuration()
+camera_config = picam2.create_preview_configuration(main={"size": (640, 480)})
 picam2.configure(camera_config)
 picam2.start()
 
