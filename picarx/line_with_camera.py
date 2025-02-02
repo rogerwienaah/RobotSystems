e7a40a37d2189df300d21c2de7896e623aa9775d
Roger Wienaah <rogerwienaah@gmail.com>
2025-02-02 15:26:23 -0800

    line follower update

--- picarx/line_with_camera.py
+++ picarx/line_with_camera.py
@@ -13,6 +13,8 @@ camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 picam2.start()
 
+px = Picarx()
+px.set_cam_tilt_angle(-45)
 
 while True:
     img = picam2.capture_array()
